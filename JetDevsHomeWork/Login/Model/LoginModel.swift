00d8127c57e10192ad55d0e7eedfcdc1c9324665
Jay Sharma <jay.sharma@theonetechnologies.co.in>
2023-08-29 15:20:10 +0530

    - Added login request model and updated response model changes to make code compilable and to fulfil swiftlint errors

--- JetDevsHomeWork/Login/Model/LoginModel.swift
+++ JetDevsHomeWork/Login/Model/LoginModel.swift
@@ -6,20 +6,35 @@
 //
 
 import Foundation
-
 struct LoginResponse: Codable {
+    
     let result: Int?
-    let error_message: String?
+    let errorMessage: String?
     let data: LoginData?
+    
+    private enum CodingKeys: String, CodingKey {
+        case result = "result"
+        case errorMessage = "error_message"
+        case data = "data"
+    }
 }
-
 struct LoginData: Codable {
+    
     let user: User?
 }
 
 struct User: Codable {
-    let user_id: Int?
-    let user_name: String?
-    let user_profile_url: String?
-    let created_at: String?
+    
+    let userId: Int?
+    let userName: String?
+    let profileImage: String?
+    let createdAt: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case userName = "user_name"
+        case profileImage = "user_profile_url"
+        case createdAt = "created_at"
+    }
+    
 }
---
+++ JetDevsHomeWork/Login/Model/LoginRequestModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginRequestModel.swift
+//  JetDevsHomeWork
+//
+//  Created by theonetech on 29/08/23.
+//
+
+import Foundation
+
+struct LoginRequestModel {
+    
+    let email: String
+    let password: String
+}
